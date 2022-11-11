9f5ac6d6d87772fb4bb228fd60b5436f373c1e4b
Tiago3670 <117348502+Fabio16295@users.noreply.github.com>
2022-11-11 16:35:02 +0000

    script

--- Processor/src/ProcessorInterface.java
+++ Processor/src/ProcessorInterface.java
@@ -5,4 +5,6 @@ import java.rmi.RemoteException;
 public interface ProcessorInterface extends Remote {
     public void Send (RequestClass r) throws IOException, InterruptedException;
     public int GetEstado() throws RemoteException;
+    public void Exec() throws IOException;
+
 }
--- Processor/src/ProcessorManager.java
+++ Processor/src/ProcessorManager.java
@@ -1,13 +1,19 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.Scanner;
 import java.util.UUID;
+
+import static java.lang.Runtime.getRuntime;
 
 public class ProcessorManager extends UnicastRemoteObject implements ProcessorInterface {
 
@@ -39,6 +45,27 @@ public class ProcessorManager extends UnicastRemoteObject implements ProcessorIn
            System.out.println("é nome do file é "+f.getName());
            System.out.println("é nome do script é "+Script.getName());
            request.setEstadoConcluido();
+           byte[] scriptfile = Base64.getDecoder().decode(Script.FileBase64().getBytes(StandardCharsets.UTF_8));
+           String scriptdecode = new String(scriptfile, StandardCharsets.UTF_8);
+           System.out.println(scriptdecode);
+
+              ProcessBuilder execut = new ProcessBuilder("cmd","/c",scriptdecode);
+               Process process = execut.start();
+        StringBuilder output = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line + System.lineSeparator());
+            }
+
+        }
+         catch (RemoteException  e) {
+        e.printStackTrace();
+        } catch (IOException e) {
+        e.printStackTrace();
+        }
+        System.out.println("->"+output);
     }
 
 
@@ -47,6 +74,14 @@ public class ProcessorManager extends UnicastRemoteObject implements ProcessorIn
             return 0;
         else
         return request.getEstado();
+    }
+
+    public void Exec() throws IOException
+    {
+        byte[] scriptfile = Base64.getDecoder().decode(Script.FileBase64().getBytes(StandardCharsets.UTF_8));
+
+   //     ProcessBuilder processBuilder = new ProcessBuilder("cmd","/c",f);
+     //   Process process = processBuilder.start();
     }
 
 
